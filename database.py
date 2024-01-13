824d8ba5ff63f1eb25c0171b0d532a743fdad096
oliverbravery <dev@oliverbravery.uk>
2024-01-13 13:04:56 +0000

    Abstracted database and model classes to their own respective files

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class Database:
+    connection: sqlite3.Connection
+    cursor = sqlite3.Cursor
+    def __init__(self):
+        self.connection = sqlite3.connect("storage.db")
+        self.cursor = self.connection.cursor()
+        self.init_db()
+    def init_db(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS areas (title TEXT, region TEXT PRIMARY KEY)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS breeders (title TEXT, owner TEXT, id TEXT PRIMARY KEY)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS members (description TEXT, id TEXT PRIMARY KEY, signatory TEXT, address TEXT, town TEXT)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS breeds (code TEXT PRIMARY KEY, id TEXT, last_litter TEXT, description TEXT, group_code TEXT, group_description TEXT)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS breeders_breeds (breeder_id TEXT, breed_code TEXT, FOREIGN KEY(breeder_id) REFERENCES breeders(id), FOREIGN KEY(breed_code) REFERENCES breeds(code), PRIMARY KEY(breeder_id, breed_code))")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS breeders_members (breeder_id TEXT, member_id TEXT, FOREIGN KEY(breeder_id) REFERENCES breeders(id), FOREIGN KEY(member_id) REFERENCES members(id), PRIMARY KEY(breeder_id, member_id))")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS areas_breeders (area_region TEXT, breeder_id TEXT, FOREIGN KEY(area_region) REFERENCES areas(region), FOREIGN KEY(breeder_id) REFERENCES breeders(id), PRIMARY KEY(area_region, breeder_id))")
+        self.connection.commit()
---
+++ models.py
@@ -0,0 +1,108 @@
+from multiprocessing import Manager
+from ctypes import c_int
+
+class Counter(object):
+    def __init__(self, manager: Manager, initval=0):
+        self.val = manager.Value(c_int, initval)
+        self.lock = manager.Lock()
+    def increment(self):
+        with self.lock:
+            self.val.value += 1
+    def decrement(self):
+        with self.lock:
+            self.val.value -= 1
+    @property
+    def value(self):
+        return self.val.value
+    
+class Breed:
+    code: str
+    id: str
+    last_litter: str
+    description: str
+    group_code: str
+    group_description: str
+    def __init__(self, code:str, id:str, last_litter:str, description:str, group_code:str, group_description:str):
+        self.code = code
+        self.id = id
+        self.last_litter = last_litter
+        self.description = description
+        self.group_code = group_code
+        self.group_description = group_description
+    def __str__(self):
+        return f"{self.code}-{self.id}-{self.last_litter}-{self.description}-{self.group_code}-{self.group_description}"
+    def __repr__(self):
+        return Breed.__str__(self)
+    
+class Member:
+    description: str
+    id: str
+    signatory: bool
+    address: str
+    town: str
+    breeder_ids: list[str]
+    def __init__(self, description: str, id: str, signatory: bool, address: str, town: str, breeder_ids: list[str] = []):
+        self.description = description
+        self.id = id
+        self.signatory = signatory
+        self.address = address
+        self.town = town
+        self.breeder_ids = breeder_ids
+    def __str__(self):
+        return f"{self.description}-{self.id}-{self.signatory}-{self.address}-{self.town}"
+    def __repr__(self):
+        return Member.__str__(self)
+    def __hash__(self) -> int:
+        return hash(self.id)
+    def __eq__(self, __value: object) -> bool:
+        return self.id == __value.id
+    
+
+class Breeder:
+    title: str
+    owner: str
+    id: str
+    area_region: str
+    breeds: list[str] #breed codes only
+    members: list[str] #member ids only
+    def __init__(self, title: str, owner: str, id: str, area_region:str, breeds: list[str] = [], members: list[str] = []):
+        self.title = title
+        self.owner = owner
+        self.id = id
+        self.area_region = area_region
+        self.breeds = breeds
+        self.members = members
+    def __hash__(self) -> int:
+        return hash(self.id)
+    def __eq__(self, __value: object) -> bool:
+        return self.id == __value.id
+    def __str__(self):
+        return f"({self.title}){self.owner}-{self.breeds}-{self.id}"
+    def __repr__(self):
+        return Breeder.__str__(self)
+    @staticmethod
+    def get_key(obj)->int:
+        return int(obj.id)
+
+class Area:
+    title: str
+    region: str
+    breeders: list[Breeder]
+    def __init__(self, title, region):
+        self.title = title
+        self.region = region
+    def __str__(self):
+        return f"({self.region}){self.title}"
+    def __repr__(self):
+        return Area.__str__(self)
+    def __hash__(self) -> int:
+        return hash(self.title, self.region)
+    def __eq__(self, o: object) -> bool:
+        return self.title == o.title and self.region == o.region
+    
+class BreedMembers:
+    breeds: list[Breed]
+    members: list[Member]
+    def __init__(self, breeds: list[Breed] = [], members: list[Member] = []):
+        self.breeds = breeds
+        self.members = members
