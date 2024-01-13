c9f89d82d513ea85101dddd5e0b904d63ed690e6
oliverbravery <dev@oliverbravery.uk>
2024-01-13 16:08:58 +0000

    Add class docstring and method docstrings

--- database.py
+++ database.py
@@ -2,13 +2,24 @@ import sqlite3
 from Progress import *
 
 class Database:
+    """
+    A class that represents a connection to a sqlite3 database.
+    """
     connection: sqlite3.Connection
     cursor = sqlite3.Cursor
+    
     def __init__(self):
+        """
+        Initializes the database connection and cursor.
+        """
         self.connection = sqlite3.connect("storage.db")
         self.cursor = self.connection.cursor()
         self.init_db()
+        
     def init_db(self):
+        """ 
+        Initializes the database tables.
+        """
         self.cursor.execute("CREATE TABLE IF NOT EXISTS areas (title TEXT, region TEXT PRIMARY KEY)")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS breeders (title TEXT, owner TEXT, id TEXT PRIMARY KEY)")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS members (description TEXT, id TEXT PRIMARY KEY, signatory TEXT, address TEXT, town TEXT)")
@@ -19,10 +30,20 @@ class Database:
         self.connection.commit()
         
     def query_no_response(self, query: str, params:tuple, progress: Progress = None) -> None:
+        """
+        Executes a query without returning a response.
+        Args:
+            query (str): The query to execute.
+            params (tuple): The parameters to pass to the query.
+            progress (Progress, optional): Progress object for tracking progress. Defaults to None.
+        """
         self.cursor.execute(query, params)
         if progress:
             progress.increment_amount_completed()
             progress.display_progress()
     
     def commit_to_database(self):
+        """
+        Commits the changes to the database.
+        """
         self.connection.commit()
